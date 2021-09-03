e702b52f75c58f2026fb75bab96e67ed8063f247
guille <guillelupianez99@gmail.com>
2021-09-03 10:48:38 +0200

    Añade env y gitignore

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_tg*)-5st3wlh^q&6n=+cgj^%_oxrw+m36s3)-*q5zs57^a09%'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
